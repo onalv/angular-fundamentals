e8ecb879b50d6241eab3abf255ca4a330d09cfa6
onalv <omarnavaalvarez@gmail.com>
2018-01-31 19:24:46 -0500

    added ngClass

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -5,16 +5,27 @@ import {Component, EventEmitter, Input, Output} from '@angular/core';
     template: `
         <div class="well hoverwell thumbnail">
             <h2>{{event.name}}</h2>
-            <div>Date: {{event.date}}</div>
-            <div>Time: {{event.time}}</div>
-            <div>Price: \${{event.price}}</div>
-            <div>
-                <span>Location: {{event.location.address}}</span>
-                <span class="pad-left">{{event.location.city}}, {{event.location.country}}</span>
+            <div>Date: {{event?.date}}</div>
+            <div [ngClass]="getStartTime()" [ngSwitch]="event?.time">
+                Time: {{event?.time}}
+                <span *ngSwitchCase="'8:00 am'">(Early start)</span>
+                <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
+                <span *ngSwitchDefault>(Normal Start)</span>
+            </div>
+            <div>Price: \${{event?.price}}</div>
+            <div *ngIf="event?.location">
+                <span>Location: {{event?.location.address}}</span>
+                <span class="pad-left">{{event?.location?.city}}, {{event?.location?.country}}</span>
+            </div>
+            <div *ngIf="event?.onlineUrl">
+                Online URL: {{event?.onlineUrl}}
             </div>
         </div>
     `,
     styles: [`
+        .green { color: #003300 !important; }
+        .bold { font-weight: bold; }
+        .thumbnail { min-height: 210px; }
         .pad-left { margin-left: 10px; }
         .well div { color: #bbb; } 
     `]
@@ -22,4 +33,9 @@ import {Component, EventEmitter, Input, Output} from '@angular/core';
 
 export class EventThumbnailComponent {
     @Input() event:any
+
+    getStartTime() {
+        const isEarlyStart = this.event && this.event.time === '8:00 am';
+        return {green: isEarlyStart, bold: isEarlyStart};
+    }
 }
