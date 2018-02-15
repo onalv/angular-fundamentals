98bd6bc929622106bf5b79afa4667b6a854c0b4d
onalv <omarnavaalvarez@gmail.com>
2018-02-15 16:20:30 -0500

    added collapsible well (common)

--- app/app.module.ts
+++ app/app.module.ts
@@ -17,6 +17,7 @@ SessionListComponent
 import { EventsAppComponent } from './events-app.component';
 import {NavBarComponent} from "./nav/navbar.component";
 import {ToastrService} from "./common/toastr.service";
+import {CollapsibleWellComponent} from "./common/collapsible-well.component";
 import {appRoutes} from "./routes";
 import {Error404Component} from "./errors/404.component";
 import {AuthService} from "./user/auth.service";
@@ -36,7 +37,8 @@ import {AuthService} from "./user/auth.service";
         CreateEventComponent,
         Error404Component,
         CreateSessionComponent,
-        SessionListComponent
+        SessionListComponent,
+        CollapsibleWellComponent
     ],
     providers: [
         EventService,
