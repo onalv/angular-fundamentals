5c287a507b13888a103f27b3ac1adbaa26787971
onalv <omarnavaalvarez@gmail.com>
2018-02-20 07:43:32 -0500

    added login

--- app/user/auth.service.ts
+++ app/user/auth.service.ts
@@ -1,18 +1,29 @@
 import {Injectable} from '@angular/core';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
 
 import {IUser} from "./user.model";
 
 @Injectable()
 export class AuthService {
-    currentUser: IUser
+    currentUser: IUser;
+
+    constructor(private http: Http) {}
 
     loginUser(userName: string, password: string) {
-        this.currentUser = {
-            id: 1,
-            userName: userName,
-            firstName: 'Omar',
-            lastName: 'Nava'
-        }
+        let headers = new Headers({'Content-Type': 'application/json'});
+        let options = new RequestOptions({headers: headers});
+        let logInfo = {username: userName, password: password};
+
+        return this.http.post('/api/login', JSON.stringify(logInfo), options)
+            .do(resp => {
+                if(resp) {
+                    this.currentUser = <IUser>resp.json().user;
+                }
+            }).catch(error => {
+                return Observable.of(false);
+            })
+
     }
 
     isAuthenticated() {
--- app/user/login.component.ts
+++ app/user/login.component.ts
@@ -11,6 +11,7 @@ import {AuthService} from "./auth.service";
 })
 
 export class LoginComponent {
+    loginInvalid = false;
 
     constructor(
         private authService: AuthService,
@@ -18,8 +19,14 @@ export class LoginComponent {
     ) {}
 
     login(formValues) {
-        this.authService.loginUser(formValues.userName, formValues.password);
-        this.router.navigate(['events']);
+        this.authService.loginUser(formValues.userName, formValues.password)
+            .subscribe(resp => {
+                if(!resp) {
+                    this.loginInvalid = true;
+                } else {
+                    this.router.navigate(['events']);
+                }
+            });
     }
 
     cancel() {
